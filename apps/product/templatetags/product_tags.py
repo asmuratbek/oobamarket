b7a2a85a5cdeee00c07e3324e9bfca55350533e7
monokbaev <monokbaev@gmail.com>
2017-04-29 18:54:20 +0600

    Done with registration and favorites pages

--- apps/product/templatetags/product_tags.py
+++ apps/product/templatetags/product_tags.py
@@ -8,41 +8,52 @@ register = template.Library()
 
 @register.assignment_tag
 def is_favorite(product, user):
-    if product.favorite.filter(user=user).exists():
-        return mark_safe('''<a class="favorite-btn active" href="">
-                    <span class="glyphicon glyphicon-heart"></span>
-                    Удалить из избранного</a>''')
-    else:
-        return mark_safe('''<a class="favorite-btn" href="">
-                    <span class="glyphicon glyphicon-heart"></span>
-                    Добавить в избранное</a>''')
+    if user.is_authenticated:
+        if product.favorite.filter(user=user).exists():
+            return mark_safe('''<a class="favorite-btn active" href="">
+                        <span class="glyphicon glyphicon-heart"></span>
+                        Удалить из избранного</a>''')
+        else:
+            return mark_safe('''<a class="favorite-btn" href="">
+                        <span class="glyphicon glyphicon-heart"></span>
+                        Добавить в избранное</a>''')
 
 
 @register.assignment_tag
 def is_in_cart(product, user):
-    if product.cartitem_set.filter(cart__user=user).exists():
-        return 'Удалить из корзины'
-    else:
-        return 'Добавить в корзину'
+    if user.is_authenticated:
+        if product.cartitem_set.filter(cart__user=user).exists():
+            return 'Удалить из корзины'
+        else:
+            return 'Добавить в корзину'
 
 @register.assignment_tag
 def is_in_cart_block(product, user):
-    if not product.cartitem_set.filter(cart__user=user).exists():
-        return mark_safe('''
-            <a href="#" class="add-basket">
-                        <span class="glyphicon glyphicon-shopping-cart"></span>
-                        Добавить в корзину
-                    </a>
-        ''')
+    if user.is_authenticated:
+        if not product.cartitem_set.filter(cart__user=user).exists():
+            return mark_safe('''
+                <a href="#" class="add-basket">
+                            <span class="glyphicon glyphicon-shopping-cart"></span>
+                            Добавить в корзину
+                        </a>
+            ''')
+        else:
+            return mark_safe('''<a href="#" class="add-basket in-the-basket">
+                            <span class="glyphicon glyphicon-shopping-cart"></span>
+                            Удалить из корзины
+                        </a>''')
     else:
-        return mark_safe('''<a href="#" class="add-basket in-the-basket">
-                        <span class="glyphicon glyphicon-shopping-cart"></span>
-                        Удалить из корзины
-                    </a>''')
+        return mark_safe('''
+                        <a href="#" class="add-basket">
+                                    <span class="glyphicon glyphicon-shopping-cart"></span>
+                                    Добавить в корзину
+                                </a>
+                    ''')
 
 @register.assignment_tag
 def is_favorite_for_like(product, user):
-    if product.favorite.filter(user=user).exists():
-        return 'like'
-    else:
-        return ''
+    if user.is_authenticated:
+        if product.favorite.filter(user=user).exists():
+            return 'like'
+        else:
+            return ''
