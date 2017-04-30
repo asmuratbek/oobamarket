8101926c096b858dc510022d20f18358414bb606
monokbaev <monokbaev@gmail.com>
2017-04-30 16:58:13 +0600

    Starting cart

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Cart
+from django.views import generic
 # Create your views here.
+
+
+class CartDetailView(generic.DetailView):
+    model = Cart
