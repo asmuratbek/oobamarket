ea1e80f45e15423da69af0ce4c9136cc4b17996a
monokbaev <monokbaev@gmail.com>
2017-05-08 18:33:18 +0600

    fix fix fix fix fix fix

--- config/settings/local.py
+++ config/settings/local.py
@@ -15,7 +15,7 @@ from .base import *  # noqa
 # ------------------------------------------------------------------------------
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
-
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['*'])
 # SECRET CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
