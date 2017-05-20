f2a830ed98dbc9cb918c3560b6b324eb39e31f55
rasul312 <rasul@googstudio.com>
2017-05-20 14:00:41 +0600

    Добавил галп в проект

---
+++ gulp/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp         	= require('gulp'),
+    sass         	= require('gulp-sass'),
+    autoprefixer 	= require('gulp-autoprefixer'),
+    cleanCSS    	= require('gulp-clean-css'),
+    rename       	= require('gulp-rename'),
+    browserSync  	= require('browser-sync').create(),
+    uglify       	= require('gulp-uglify'),
+    del 			= require('del');
+
+gulp.task('styles', function () {
+    return gulp.src('gulp/sass/*.sass')
+        .pipe(sass({
+            includePaths: require('node-bourbon').includePaths
+        }).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix : ''}))
+        .pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('static/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('resources/assets/sass/*.sass', ['styles']);
+});
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+
+gulp.task('default', ['watch']);
