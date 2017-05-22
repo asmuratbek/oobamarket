5d91baf7d2aea963790f4e404fe872f9e398813f
Muratbek <asmuratbek@gmail.com>
2017-05-22 21:10:30 +0600

    add & update shop forms is styled

--- apps/shop/templatetags/shop_tags.py
+++ apps/shop/templatetags/shop_tags.py
@@ -10,9 +10,20 @@ def show_add_product_button(shop, user):
     if user.is_authenticated:
         if shop.is_owner(user):
             return mark_safe('''
-                            <li class="active">
-                            <a href="%s">
-                            Добавить Товары</a></li>
+                            <div class="col-md-4 col-sm-6">
+                            <div class="cover">
+                                <a class="url-item" href="%s"></a>
+                                <div class="add-product">
+                                    <i class="glyphicon glyphicon-plus-sign"></i>
+                                    <p>Добавить новый товар</p>
+                                </div>
+
+                                <div class="stock">
+                                    <a href="#">Добавить акцию</a>
+                                </div>
+                            </div>
+                        </div>
                                 ''' % reverse('product:add_product', kwargs={'slug':shop.slug}))
         else:
             return mark_safe('')
+
