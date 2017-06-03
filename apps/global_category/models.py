37b4b771187e5eb61ece14508bcaebb6f1fb415a
monokbaev <monokbaev@gmail.com>
2017-06-03 19:56:57 +0600

    changed max_length for slug and title in global_category

--- apps/global_category/models.py
+++ apps/global_category/models.py
@@ -10,8 +10,8 @@ class GlobalCategory(models.Model):
         verbose_name = 'Глобальная категория'
         verbose_name_plural = 'Глобальные категории'
 
-    title = models.CharField(max_length=14, verbose_name='Название категории')
-    slug = models.CharField(max_length=32, verbose_name='Название на транслите')
+    title = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.CharField(max_length=255, verbose_name='Название на транслите')
     icon = models.ImageField(upload_to="category/icons", default=None)
     created_at = models.DateTimeField(auto_now=True, verbose_name="Создано")
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Обновлено")
