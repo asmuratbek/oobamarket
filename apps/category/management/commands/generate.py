4600a05293d1530bf52dafcf0ef716533582d922
monokbaev <monokbaev@gmail.com>
2017-06-03 18:40:13 +0600

    added integrity error

--- apps/category/management/commands/generate.py
+++ apps/category/management/commands/generate.py
@@ -2,24 +2,12 @@ import random
 
 from django.core.management.base import BaseCommand, CommandError
 from slugify import slugify
-
+from django.db.utils import IntegrityError
 from apps.category.models import Category
 from apps.global_category.models import GlobalCategory
 from django.conf import settings
 import openpyxl as px
 from multiprocessing import Pool
-
-"""import openpyxl as px
-import numpy as np
-
-W = px.load_workbook('filename.xlsx', use_iterators = True)
-p = W.get_sheet_by_name(name = 'Sheet1')
-
-a=[]
-
-for row in p.iter_rows():
-    for k in row:
-        a.append(k.internal_value)"""
 
 
 class Command(BaseCommand):
@@ -45,7 +33,7 @@ class Command(BaseCommand):
                                 slug = slugify(title)
                                 Category.objects.create(title=title, slug=slug, section=section)
                                 self.stdout.write(self.style.SUCCESS('Создана категория-родитель "%s"' % title))
-                            except:
+                            except IntegrityError:
                                 title = field.internal_value[3:]
                                 random_int = random.randrange(0, 101)
                                 slug = slugify(title) + str(random_int)
@@ -58,7 +46,7 @@ class Command(BaseCommand):
                                 slug = slugify(field.internal_value)
                                 Category.objects.create(title=field.internal_value, slug=slug, section=section, parent=last_parent)
                                 self.stdout.write(self.style.SUCCESS('Создана категория "%s"' % field.internal_value))
-                            except:
+                            except IntegrityError:
                                 parents = Category.objects.filter(parent=None).order_by('created_at')
                                 last_parent = parents.last()
                                 random_int = random.randrange(0, 1010)
@@ -66,20 +54,3 @@ class Command(BaseCommand):
                                 Category.objects.create(title=field.internal_value, slug=slug, section=section,
                                                         parent=last_parent)
                                 self.stdout.write(self.style.SUCCESS('Создана категория "%s"' % field.internal_value))
-        # a = []
-        #
-        # for row in p.iter_rows():
-        #     for k in row:
-        #         a.append(k.internal_value)
-        # file = open(settings.MEDIA_ROOT + '/' + str(type) + '.xlsx')
-        # print(file)
-        # for type in options['type']:
-        #     try:
-        #         poll = Category.objects.get(pk=type)
-        #     except Category.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % type)
-        #
-        #     poll.opened = False
-        #     poll.save()
-        #
-        #     self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % type))
