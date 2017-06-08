5eb7e5c0aea93e946cd2b29c043f8e17dbe216d0
monokbaev <monokbaev@gmail.com>
2017-06-08 19:28:44 +0600

    Created Shop component

---
+++ web_client/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import createClass from 'create-react-class';
+
+
+var Shop = createClass({
+  displayName: 'Shop',
+
+  render: function(){
+    return(
+      
+    )
+  }
+});
