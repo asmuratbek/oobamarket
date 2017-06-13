577673123e7dc8a5502bd3e28e5861159315a78b
monokbaev <monokbaev@gmail.com>
2017-06-13 20:07:41 +0000

    changed output if category has no property

--- static/js/common.js
+++ static/js/common.js
@@ -222,6 +222,7 @@ $(document).ready(function () {
                 success: function (data) {
                     console.log(data);
                     $('#property_list').append(data);
+                    $('#empty_properties').fadeOut();
 
                 },
                 error: function (response, error) {
