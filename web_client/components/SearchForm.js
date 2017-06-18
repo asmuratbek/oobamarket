0eb68226cc75569164bd831cf4c1733d8a10addc
monokbaev <monokbaev@gmail.com>
2017-06-18 22:05:58 +0000

    Added some features

--- web_client/components/SearchForm.js
+++ web_client/components/SearchForm.js
@@ -5,17 +5,21 @@ import createClass from 'create-react-class';
 var SearchForm = createClass({
   displayName: 'SearchForm',
 
+  handleSort: function(e) {
+    this.props.onReOrder(e.target.value);
+  }, //handleSort
+
   render: function(){
     return (
       <div className="form-filter">
                 <form className="form-inline">
 
                     <div className="form-group select">
-                        <select className="form-control">
-                            <option>Сортировка</option>
-                            <option>Цена по возрастанию</option>
-                            <option>Цена по убыванию</option>
-                            <option>Сначала новые</option>
+                        <select className="form-control" value={this.props.orderBy} onChange={this.handleSort}>
+                            <option value="title">По названию</option>
+                            <option value="priceAsc">Цена по возрастанию</option>
+                            <option value="priceDesc">Цена по убыванию </option>
+                            <option value="newFirst">Сначала новые </option>
                         </select>
                     </div>
 
