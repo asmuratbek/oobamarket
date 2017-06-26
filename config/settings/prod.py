f91b294050ba52a1fa198a6b10ec3482cc90ac8f
monokbaev <monokbaev@gmail.com>
2017-06-26 16:02:12 +0000

    fix

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -139,7 +139,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 
 # Use the Heroku-style specification
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
-DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES['default'] = 'postgresql:///market'
 
 # CACHING
 # ------------------------------------------------------------------------------
