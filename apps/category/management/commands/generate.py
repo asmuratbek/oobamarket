9df4bbd0a5168487b8b1f8af79c97837241bc59a
monokbaev <monokbaev@gmail.com>
2017-07-04 11:19:26 +0000

    generate command in progress

--- apps/category/management/commands/generate.py
+++ apps/category/management/commands/generate.py
@@ -12,9 +12,66 @@ import openpyxl as px
 class Command(BaseCommand):
     help = 'Creates model instances from xlsx'
 
-    def add_arguments(self, parser):
-        parser.add_argument('type', type=str)
+    def handle(self, *args, **options):
+        W = px.load_workbook(settings.MEDIA_ROOT + '/ooba.xlsx')
+        for index, worksheet in enumerate(W.worksheets[1:3]):
+            if index == 0:
+                wiki = dict()
+                for row in worksheet.iter_rows():
+                    for index, field in enumerate(row):
+                        if field.value:
+                            if index == 0:
+                                key = field.value
+                                wiki[key] = []
+                            else:
+                                wiki[key].append(field.value)
 
+            else:
+                for col in worksheet.iter_cols():
+                    for index, field in enumerate(col):
+                        if field.value:
+                            if field.internal_value[0].isdigit():
+                                x = str(field.internal_value).split('.')[:-1]
+                                x.append(str(field.internal_value).split('.')[-1][0])
+                                print(x)
+
+            #  field.value.split('.')[-1][0]
+            # slug = slugify(worksheet.title)
+            # section, created = GlobalCategory.objects.get_or_create(title=worksheet.title, slug=slug)
+            # worksheet = W.get_sheet_by_name(name=worksheet.title)
+            # for row in worksheet.iter_rows(max_col=1):
+            #     for field in row:
+            #         if field.internal_value:
+            #             if field.internal_value[0].isdigit():
+            #                 try:
+            #                     title = field.internal_value[3:]
+            #                     slug = slugify(title)
+            #                     Category.objects.create(title=title, slug=slug, section=section)
+            #                     self.stdout.write(self.style.SUCCESS('Создана категория-родитель "%s"' % title))
+            #                 except IntegrityError:
+            #                     title = field.internal_value[3:]
+            #                     random_int = random.randrange(0, 101)
+            #                     slug = slugify(title) + str(random_int)
+            #                     Category.objects.create(title=title, slug=slug, section=section)
+            #                     self.stdout.write(self.style.SUCCESS('Создана категория-родитель "%s"' % title))
+            #             else:
+            #                 try:
+            #                     parents = Category.objects.filter(parent=None).order_by('created_at')
+            #                     last_parent = parents.last()
+            #                     slug = slugify(field.internal_value) + '-' + str(last_parent.id)
+            #                     Category.objects.create(title=field.internal_value, slug=slug, section=section, parent=last_parent)
+            #                     self.stdout.write(self.style.SUCCESS('Создана категория "%s"' % field.internal_value))
+            #                 except IntegrityError:
+            #                     parents = Category.objects.filter(parent=None).order_by('created_at')
+            #                     last_parent = parents.last()
+            #                     random_int = random.randrange(0, 1010)
+            #                     slug = slugify(field.internal_value) + '-' + str(last_parent.id)
+            #                     Category.objects.create(title=field.internal_value, slug=slug, section=section,
+            #                                             parent=last_parent)
+            #                     self.stdout.write(self.style.SUCCESS('Создана категория "%s"' % field.internal_value))
+
+
+"""
     def handle(self, *args, **options):
         type = options['type']
         W = px.load_workbook(settings.MEDIA_ROOT + '/' + str(type) + '.xlsx')
@@ -51,28 +108,29 @@ class Command(BaseCommand):
                                 slug = slugify(field.internal_value) + '-' + str(last_parent.id)
                                 Category.objects.create(title=field.internal_value, slug=slug, section=section,
                                                         parent=last_parent)
-                                self.stdout.write(self.style.SUCCESS('Создана категория "%s"' % field.internal_value))
+                                self.stdout.write(self.style.SUCCESS('Создана категория "%s"' % field.internal_value))"""
 
 
-def parse_all_file():
-    file_load = px.load_workbook(settings.MEDIA_ROOT + "/" + 'categories.xlsx')
-    for worksheet in file_load.worksheets[1:]:
-        slug = slugify(worksheet.title)
-        section = GlobalCategory.objects.get_or_create(title=worksheet.title, slug=slug)
-        worksheet = file_load.get_sheet_by_name(name=worksheet.title)
-        for row in worksheet.iter_rows('A{}:A{}'.format(worksheet.min_row, worksheet.max_row)):
-            for field in row:
-                if field.internal_value is not None and isinstance(field.internal_value[0], int):
-                    try:
-                        title = field.internal_value[3:]
-                        slug = slugify(title)
-                        Category.objects.create(title=title, slug=slug, section=section)
-                        print("Создана категория-родитель %s" % title)
-                        # self.stdout.write(self.style.SUCCESS('Создана категория-родитель "%s"' % title))
-                    except IntegrityError:
-                        title = field.internal_value[3:]
-                        random_int = random.randrange(0, 101)
-                        slug = slugify(title) + str(random_int)
-                        Category.objects.create(title=title, slug=slug, section=section)
-                        print("Создана категория-родитель %s" % title)
-                        # self.stdout.write(self.style.SUCCESS('Создана категория-родитель "%s"' % title))
+
+# def parse_all_file():
+#     file_load = px.load_workbook(settings.MEDIA_ROOT + "/" + 'categories.xlsx')
+#     for worksheet in file_load.worksheets[1:]:
+#         slug = slugify(worksheet.title)
+#         section = GlobalCategory.objects.get_or_create(title=worksheet.title, slug=slug)
+#         worksheet = file_load.get_sheet_by_name(name=worksheet.title)
+#         for row in worksheet.iter_rows('A{}:A{}'.format(worksheet.min_row, worksheet.max_row)):
+#             for field in row:
+#                 if field.internal_value is not None and isinstance(field.internal_value[0], int):
+#                     try:
+#                         title = field.internal_value[3:]
+#                         slug = slugify(title)
+#                         Category.objects.create(title=title, slug=slug, section=section)
+#                         print("Создана категория-родитель %s" % title)
+#                         # self.stdout.write(self.style.SUCCESS('Создана категория-родитель "%s"' % title))
+#                     except IntegrityError:
+#                         title = field.internal_value[3:]
+#                         random_int = random.randrange(0, 101)
+#                         slug = slugify(title) + str(random_int)
+#                         Category.objects.create(title=title, slug=slug, section=section)
+#                         print("Создана категория-родитель %s" % title)
+#                         # self.stdout.write(self.style.SUCCESS('Создана категория-родитель "%s"' % title))
