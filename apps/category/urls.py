5b5b05f69a6ba3c13927938c4fe8aeecf9458517
Daniyar <donnietruth@gmail.com>
2017-07-18 19:58:29 +0600

    Ignore dots in url

--- apps/category/urls.py
+++ apps/category/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     url(r'^get_subcategory_list', get_subcategory_from_category, name="subcategory_list_ajax"),
     url(r'^get_property_list', get_property_list, name="property_list_ajax"),
     url(r'^get_product', get_product_by_filter, name='filter_detail'),
-    url(r'^(?P<global_slug>[\w-]+)/(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/', include('apps.product.urls')),
-    url(r'^(?P<global_slug>[\w-]+)/(?P<slug>[\w-]+)/$', category_detail, name='detail'),
+    url(r'^(?P<global_slug>[\w-]+)/(?P<category_slug>[.\w-]+)/(?P<slug>[\w-]+)/', include('apps.product.urls')),
+    url(r'^(?P<global_slug>[\w-]+)/(?P<slug>[.\w-]+)/$', category_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/$', GlobalDetailView.as_view(), name='global_detail'),
 
 
