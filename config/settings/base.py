a4613283ab404403397f3886bdd28026a29edac8
Daniyar <donnietruth@gmail.com>
2017-07-24 17:34:49 +0600

    Add validate error in login page

--- config/settings/base.py
+++ config/settings/base.py
@@ -176,7 +176,7 @@ DATABASES['default']['ATOMIC_REQUESTS'] = True
 TIME_ZONE = 'Asia/Bishkek'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
