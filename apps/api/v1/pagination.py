21f8331f76236643f787089693b960a008b183be
monokbaev <monokbaev@gmail.com>
2017-08-09 12:54:46 +0600

    Pagination at all products page

--- apps/api/v1/pagination.py
+++ apps/api/v1/pagination.py
@@ -8,7 +8,7 @@ class CategoryLimitPagination(PageNumberPagination):
 
 
 class ProductLimitPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 40
 
