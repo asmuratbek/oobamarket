20f626e8fa0a673c3d0bbde84cfa83146249c5f6
Daniyar <donnietruth@gmail.com>
2017-08-30 23:23:43 +0600

    Some changes in fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,12 @@
-from fabric.api import cd, env, prefix, put, prompt, local, sudo, run
+from fabric.api import cd, env, prefix, put, prompt, local, sudo, run, task
 
 
 PROJECT_NAME = 'ooba'
 PROJECT_ROOT = '/home/admin/%s' % PROJECT_NAME
 VENV_DIR = "/home/admin/ooba_env"
 REPO = 'git@bitbucket.org:monokbaev/oobamarket.git'
+STAGING_NAME = 'ooba_dev'
+STAGING_ROOT = '/home/admin/ooba_dev'
 
 
 env.hosts = ['176.31.28.85']
@@ -20,3 +22,13 @@ def update():
             run('./manage.py collectstatic --noinput')
             run('./manage.py migrate --settings=config.settings.prod')
             run('supervisorctl restart ooba_market')
+
+
+def update_dev():
+    with cd(STAGING_ROOT):
+        local('git pull')
+        with prefix('source ' + VENV_DIR + '/bin/activate'):
+            run('pip install -r requirements/production.txt')
+            run('./manage.py collectstatic --noinput')
+            run('./manage.py migrate --settings=config.settings.prod')
+            run('supervisorctl restart ooba_dev')
