5a61e47d699f59aea01a495d9a08e6a0ab1392e5
Daniyar <donnietruth@gmail.com>
2017-08-31 12:34:28 +0600

    change wsgi settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -30,7 +30,10 @@ if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.prod':
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
+if os.path.split(app_path)[-1] == "ooba":
+    os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.prod"
+else:
+    os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.dev"
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
