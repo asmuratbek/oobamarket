1f3cbc12f850dc4fd06a82d4217e14de5fed6f9e
Daniyar <donnietruth@gmail.com>
2017-09-10 12:17:31 +0600

    fix in fabfile

--- fabfile.py
+++ fabfile.py
@@ -10,28 +10,28 @@ STAGING_NAME = 'oobamarket'
 STAGING_ROOT = '/home/admin_dev/oobamarket'
 
 
-env.hosts = ['176.31.28.85']
-env.user = 'root'
-env.password = '81Qm0yhhaSLd'
-
-
 def update():
+    env.host_string = '176.31.28.85'
+    env.user = 'admin'
+    env.password = 'sunrise226417'
     with cd(PROJECT_ROOT):
-        local('git pull {}'.format(REPO))
+        run('git pull origin master')
         with prefix('source ' + VENV_DIR + '/bin/activate'):
             run('pip install -r requirements/production.txt')
             run('./manage.py collectstatic --noinput')
             run('./manage.py migrate --settings=config.settings.prod')
-            run('supervisorctl restart ooba_market')
+            sudo('supervisorctl restart ooba_market')
 
 
 def update_dev():
+    env.host_string = '145.239.33.4'
+    env.user = 'admin_dev'
+    env.password = 'sunrise226417'
     with cd(STAGING_ROOT):
-        with settings(user='admin_dev', password='sunrise226417'):
-            local("git pull origin dev")
-            with prefix('source ' + VENV_DEV_DIR + '/bin/activate'):
-                run('pip install -r requirements/production.txt')
-                run('./manage.py collectstatic --noinput')
-                run('./manage.py migrate --settings=config.settings.dev')
-                sudo('service supervisor stop')
-                sudo('service supervisor start')
+        run("git pull origin dev")
+        with prefix('source ' + VENV_DEV_DIR + '/bin/activate'):
+            run('pip install -r requirements/production.txt')
+            run('./manage.py collectstatic --noinput')
+            run('./manage.py migrate --settings=config.settings.dev')
+            sudo('service supervisor stop')
+            sudo('service supervisor start')
