febfb68b08e9e6c2551cd2ea41ec317fff38d308
Daniyar <donnietruth@gmail.com>
2017-09-10 11:17:09 +0600

    some fix in fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,9 +4,10 @@ from fabric.api import cd, env, prefix, put, prompt, local, sudo, run, task
 PROJECT_NAME = 'ooba'
 PROJECT_ROOT = '/home/admin/%s' % PROJECT_NAME
 VENV_DIR = "/home/admin/ooba_env"
+VENV_DEV_DIR = "/home/admin_dev/ooba_env"
 REPO = 'git@bitbucket.org:monokbaev/oobamarket.git'
-STAGING_NAME = 'ooba_dev'
-STAGING_ROOT = '/home/admin/ooba_dev'
+STAGING_NAME = 'oobamarket'
+STAGING_ROOT = '/home/admin_dev/oobamarket'
 
 
 env.hosts = ['176.31.28.85']
@@ -27,8 +28,9 @@ def update():
 def update_dev():
     with cd(STAGING_ROOT):
         local("git pull origin dev")
-        with prefix('source ' + VENV_DIR + '/bin/activate'):
+        with prefix('source ' + VENV_DEV_DIR + '/bin/activate'):
             run('pip install -r requirements/production.txt')
             run('./manage.py collectstatic --noinput')
-            run('./manage.py migrate --settings=config.settings.prod')
-            run('supervisorctl restart ooba_market_dev')
+            run('./manage.py migrate --settings=config.settings.dev')
+            sudo('service supervisor stop')
+            sudo('service supervisor start')
