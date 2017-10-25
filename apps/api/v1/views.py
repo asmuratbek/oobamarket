0d562b45b25324c0078f1324e4827cfcab3e420a
Daniyar <donnietruth@gmail.com>
2017-10-25 11:40:03 +0600

    fix bug in lenta view api

--- apps/api/v1/views.py
+++ apps/api/v1/views.py
@@ -127,8 +127,8 @@ class LentaView(APIView):
                     })
                 else:
                     items.append(dict(shop=item.shop.title, title=item.title, short_description=item.short_description,
-                                      description=item.description, discount=item.discount, image=item.image.url,
-                                      type="sale"))
+                                      description=item.description, discount=item.discount,
+                                      image=item.image.url if item.image else None, type="sale"))
         return JsonResponse({
             "status": "success",
             "page": page if page else 1,
