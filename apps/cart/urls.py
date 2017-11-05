1fe3c4f8bad1c8912a1903379d5874f33fc58ac1
Daniyar <donnietruth@gmail.com>
2017-11-05 18:28:25 +0600

    added regular symbols in url

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -3,8 +3,8 @@ from .views import CartDetailView, CartDetailByPkUser, CartDetailByPkShop, Confi
 
 
 urlpatterns = [
-    url(r'^user/(?P<username>[\w-]+)/(?P<pk>\d+)/$', CartDetailByPkUser.as_view(), name='detail_by_pk_user'),
-    url(r'^shop/(?P<shop_slug>[\w-]+)/(?P<pk>\d+)/$', CartDetailByPkShop.as_view(), name='detail_by_pk_shop'),
+    url(r'^user/(?P<username>[\w.@+-]+)/(?P<pk>\d+)/$', CartDetailByPkUser.as_view(), name='detail_by_pk_user'),
+    url(r'^shop/(?P<shop_slug>[\w.@+-]+)/(?P<pk>\d+)/$', CartDetailByPkShop.as_view(), name='detail_by_pk_shop'),
     url(r'^(?P<pk>\d+)/confirm/$', ConfirmOrderByShop.as_view(), name='detail_by_pk'),
     url(r'^$', CartDetailView.as_view(), name='detail'),
 ]
